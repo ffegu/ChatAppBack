8991cd8941938c841ff4490f7932e614be38d78b
Fran Fegu <ffegu0617@gmail.com>
2022-02-18 17:01:13 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -3,46 +3,46 @@ const cors = require("cors")
 const app = express()
 const http = require("http")
 const server = http.createServer(app)
-const { Server } = require("socket.io")
+// const { Server } = require("socket.io")
 const moment = require("moment")
 
 app.use(cors())
-const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-})
+// const io = new Server(server, {
+//     cors: {
+//         origin: "*"
+//     }
+// })
 app.use(express.static(__dirname + '/public'))
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
-const users = []
+// const users = []
 
-io.on("connection", (socket) => {
+// io.on("connection", (socket) => {
 
-    socket.send("update-id")
+//     socket.send("update-id")
 
-    socket.on("new-user", user => {
-        users[socket.id] = { id: socket.id, ...user }
-        socket.broadcast.emit('user-connected', users[socket.id])
-    })
+//     socket.on("new-user", user => {
+//         users[socket.id] = { id: socket.id, ...user }
+//         socket.broadcast.emit('user-connected', users[socket.id])
+//     })
 
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('user-disconnected', socket.id)
-    });
+//     socket.on('disconnect', () => {
+//         socket.broadcast.emit('user-disconnected', socket.id)
+//     });
 
-    socket.on("message", data => {
-        socket.to(data?.user?.id).emit("new-message", { text: data.message, time: moment().format('h:mm A'), user: users[socket.id] })
-    })
+//     socket.on("message", data => {
+//         socket.to(data?.user?.id).emit("new-message", { text: data.message, time: moment().format('h:mm A'), user: users[socket.id] })
+//     })
 
-    socket.on("typing", data => {
-        socket.to(data.user.id).emit("typing", {
-            ...data,
-            user: users[socket.id],
-        })
-    })
-})
+//     socket.on("typing", data => {
+//         socket.to(data.user.id).emit("typing", {
+//             ...data,
+//             user: users[socket.id],
+//         })
+//     })
+// })
 
 server.listen(process.env.PORT || 3000, () => {
     console.log("Listening on http://localhost:3000")
